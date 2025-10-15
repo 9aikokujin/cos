d67ee34eb424906e2bf1f783ea522843ae4056a0
gaijin <gaijin@klikdev.click>
2025-10-15 17:43:49 +0300

    на прод

--- services/rest/app/schemas/videohistory.py
+++ services/rest/app/schemas/videohistory.py
@@ -32,7 +32,8 @@ class HistoryParams(BaseModel):
     channel_id: Optional[int] = None
     channel_type: Optional[str] = None
     user_id: Optional[int] = None
-    articles: Optional[List[str]] = Query(None, description="Список артикулов через запятую или несколько параметров")
+    articles: Optional[List[str]] = Query(
+        None, description="Список артикулов через запятую или несколько параметров")
     date_published_to: Optional[datetime] = None
     date_published_from: Optional[datetime] = None
 
